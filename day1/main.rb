213658f06d5e89123e5a575fd608703ce2ecbd1d
Josh Gaber <j.gaber@vehikl.com>
2021-12-01 09:44:04 -0500

    Day 1 complete

--- day1/main.rb
+++ day1/main.rb
@@ -1,15 +1,15 @@
 module Day1
   class Main
     def initialize(input)
-
+      @depths = input.strip.split(/\n/).map(&:to_i)
     end
 
     def part1
-
+      (1...@depths.length).count { |i| @depths[i] > @depths[i - 1] }
     end
 
     def part2
-
+      (3...@depths.length).count { |i| @depths[(i - 2)..i].sum > @depths[(i - 3)..(i - 1)].sum }
     end
   end
 end
