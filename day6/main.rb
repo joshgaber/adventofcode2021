cd05e876de637f97cddb65890d81c14f602559d9
Josh Gaber <j.gaber@vehikl.com>
2021-12-06 09:54:25 -0500

    Day 6 complete

--- day6/main.rb
+++ day6/main.rb
@@ -1,15 +1,19 @@
 module Day6
   class Main
     def initialize(input)
+      @fishes = input.split(/,/).map(&:to_i)
 
+      @spawn_counts = 256.times.each_with_object(Hash.new(1)) do |i, counts|
+        counts[i] += i.step(1, -7).sum { |j| counts[j - 9] }
+      end
     end
 
     def part1
-
+      @fishes.sum { |fish| @spawn_counts[80 - fish] }
     end
 
     def part2
-
+      @fishes.sum { |fish| @spawn_counts[256 - fish] }
     end
   end
 end
